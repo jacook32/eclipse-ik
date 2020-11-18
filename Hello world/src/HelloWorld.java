0af9e562e72d9bcc0f922c1e5aa414c6f749704f
IS KANG <rkddlstjd109@naver.com>
2020-11-18 21:16:09 +0900

    컴소공

--- Hello world/src/HelloWorld.java
+++ Hello world/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("IsKang");
-
+		System.out.println("컴퓨터 소프트웨어공학과");
 	}
 
 }
